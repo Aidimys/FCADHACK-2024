04c013b100d1ffec356208e15031d17608fbddfb
Van-horn <sashabon010@gmail.com>
2024-04-05 16:06:45 +0300

    App structure, simple back app, favicon

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const activityDate = require('./data/activity.json');
+
+const app = express();
+app.use(cors());
+
+app.get('/activity', (req, res) => {
+  setTimeout(() => res.json(activityDate), 2000);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('OK');
+});
