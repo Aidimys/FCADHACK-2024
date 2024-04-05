1052574bf878f249c8f9db6863b1d79b501d4a8d
Van-horn <sashabon010@gmail.com>
2024-04-05 22:42:48 +0300

    Loading before site rendering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import HomeHeader from './components/HomePage/HomeHeader/HomeHeader';
 import HomeMain from './components/HomePage/HomeMain/HomeMain';
@@ -11,79 +12,92 @@ import BreadHeader from './components/BreadHeader/BreadHeader';
 import SingleStudent from './components/Students/SingleStudent/SingleStudent';
 import Organizations from './components/Organizations/Organizations/Organizations';
 import SingleOrganization from './components/Organizations/SingleOrganization/SingleOrganization';
+import MainIsLoading from './components/MainIsLoading/MainIsLoading';
+//import {
+// setIsMainLoaded,
+// selectorIsMainLoaded,
+//} from './redux/slices/isMainLoadedSlice';
 
 function App() {
+  // const dispatch = useDispatch();
+  //const isLoaded = useSelector(selectorIsMainLoaded);
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
     window.onload = () => {
-      setIsLoaded(true);
+      setTimeout(() => setIsLoaded(true), 0);
     };
   }, []);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          index
-          element={
-            <>
-              <HomeHeader />
-              <HomeMain />
-              <Footer />
-            </>
-          }
-        />
-        <Route
-          path="/account"
-          element={
-            <>
-              <BreadHeader />
-              <Account />
-              <Footer />
-            </>
-          }
-        />
-        <Route
-          path="/students"
-          element={
-            <>
-              <HomeHeader />
-              <Students />
-              <Footer />
-            </>
-          }
-        />
-        <Route
-          path="/students/:studentIв"
-          element={
-            <>
-              <BreadHeader />
-              <SingleStudent />
-              <Footer />
-            </>
-          }
-        />
-        <Route
-          path="/organizations"
-          element={
-            <>
-              <HomeMain />
-              <Organizations />
-              <Footer />
-            </>
-          }
-        />
-        <Route
-          path="/organizations/:organizationSlug"
-          element={
-            <>
-              <BreadHeader />
-              <SingleOrganization />
-              <Footer />
-            </>
-          }
-        />
-        <Route path="*" element={<NotFound />} />
+        {isLoaded ? (
+          <>
+            <Route
+              index
+              element={
+                <>
+                  <HomeHeader />
+                  <HomeMain />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/account"
+              element={
+                <>
+                  <BreadHeader />
+                  <Account />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/students"
+              element={
+                <>
+                  <HomeHeader />
+                  <Students />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/students/:studentIв"
+              element={
+                <>
+                  <BreadHeader />
+                  <SingleStudent />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/organizations"
+              element={
+                <>
+                  <HomeMain />
+                  <Organizations />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/organizations/:organizationSlug"
+              element={
+                <>
+                  <BreadHeader />
+                  <SingleOrganization />
+                  <Footer />
+                </>
+              }
+            />
+            <Route path="*" element={<NotFound />} />
+          </>
+        ) : (
+          <Route path="*" element={<MainIsLoading action="waiting" />} />
+        )}
       </Routes>
     </BrowserRouter>
   );
